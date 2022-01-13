bc24efb15367f5b645ca1ce3362b045892bffd8c
Asma Sadia <a.sadia51299@gmail.com>
2022-01-12 19:16:20 -0500

    Add files via upload

---
+++ cred.py
@@ -0,0 +1,4 @@
+spotify_client_id = "55d3243d16304b7891c16f62eb45fe3c" 
+spotify_client_secret = "1c496ef4eac24b40ab5ae71c8711e554"
+spotify_redirect_url = "http://localhost:5000/callback"
+password="sadia123"
